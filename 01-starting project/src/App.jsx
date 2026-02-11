fc5977af954f326a9f3889c3843308d7fa0ad302
Margaret Mugo <Margaret.Mugo@epra.go.ke>
2026-02-11 11:57:54 +0300

    changes to the components folder

--- 01-starting project/src/App.jsx
+++ 01-starting project/src/App.jsx
@@ -3,6 +3,7 @@ import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data.js';
 import Header  from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
   return (
@@ -22,7 +23,12 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
         </section>
-
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
