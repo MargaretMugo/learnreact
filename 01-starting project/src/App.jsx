823dfe4bce62d955289a2de539a3dc8be4bb36b2
Margaret Mugo <Margaret.Mugo@epra.go.ke>
2026-02-23 13:26:32 +0300

    react essentials deep dive for components

--- 01-starting project/src/App.jsx
+++ 01-starting project/src/App.jsx
@@ -1,54 +1,15 @@
-import { useState } from 'react';
-import componentsImg from './assets/components.png';
-import { CORE_CONCEPTS } from './data.js';
-import { EXAMPLES } from './data.js';
-import Header  from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 function App() {
-  // const stateArray= useState('Please click a button');
-  const [ selectedTopic, setSelectedTopic ]= useState('components');
-  let tabContent = 'Please click a button'
-  function handleSelect(selectedButton){
-    //selectedButton -> 'components', 'jsx', 'props', 'state' 
-    setSelectedTopic(selectedButton);
-    console.log(selectedTopic);
-  }
-  console.log('APP COMPONENT RENDERING');
+  
   return (
     <div>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
-        </section>
+        <CoreConcepts/>
+        <Examples/>
       </main>
     </div>
   );
