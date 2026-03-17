f46bee674ac04091adcc96fe718deadf80be5fd7
MargaretMugo <mugomargaret128@gmail.com>
2026-03-17 11:19:26 +0300

    adding an event to handle changes in names

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -7,12 +7,12 @@ export default function Player({ initialName, symbol }) {
         setIsEditing(isEditing => !isEditing);
     }
     function handleChange(event){
-        setPlayerName(event.target);
+        setPlayerName(event.target.value);
     }
     let editablePlayerName = <span className='player-name'>{playerName}</span>
     let btnCaption = 'Edit';
     if (isEditing) {
-        editablePlayerName = <input type='text' required = {playerName} onChange={handleChange} />
+        editablePlayerName = <input type='text' required value = {playerName} onChange={handleChange} />
         btnCaption = 'Save';
     }
     return (
