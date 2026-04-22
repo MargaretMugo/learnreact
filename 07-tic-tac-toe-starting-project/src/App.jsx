e34873675de02e01c9e2dd59b44ce350974152da
MargaretMugo <mugomargaret128@gmail.com>
2026-04-22 16:41:41 +0300

    trying to add turn taking

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,23 +1,35 @@
-import { useState} from 'react';
+import { useState } from 'react';
 import Player from "./components/Player";
 import Gameboard from "./components/Gameboard";
+import Log from './components/Log';
+
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare(){
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(
+      (prevTurns) => {
+        let currentPlayer = 'X';
+        if(prevTurns[0].player === 'X'){
+          
+        }
+        const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player : activePlayer }, ...prevTurns]
+      }//...prevTurns copies the existing prevTurns
+    );
   }
   return (
     <main>
       <div id='game-container'>
         {/* Players */}
-        <ol id='players' className = 'highlight-player'>
-          <Player name='Player 1' symbol='X' isActive={activePlayer === 'X'}/>
-          <Player name='Player 2' symbol='O' isActive={activePlayer === 'O'}/>
+        <ol id='players' className='highlight-player'>
+          <Player name='Player 1' symbol='X' isActive={activePlayer === 'X'} />
+          <Player name='Player 2' symbol='O' isActive={activePlayer === 'O'} />
         </ol>
-        <Gameboard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <Gameboard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
-      {/* Log */}
+      <Log />
     </main>
   )
 }
