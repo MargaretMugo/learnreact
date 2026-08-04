6c986d16c21f0eaaed16e219c02bfd4ef28b4c3d
MargaretMugo <mugomargaret128@gmail.com>
2026-08-04 11:23:33 +0300

    punctuation changes

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -9,17 +9,17 @@ export default function Player({ initialName, symbol, isActive }) {
     function handleChange(event){
         setPlayerName(event.target.value);
     }
-    let editablePlayerName = <span className='player-name'>{playerName}</span>
-    let btnCaption = 'Edit';
+    let editablePlayerName = <span className="player-name">{playerName}</span>
+    let btnCaption = "Edit";
     if (isEditing) {
-        editablePlayerName = <input type='text' required value = {playerName} onChange={handleChange} />
-        btnCaption = 'Save';
+        editablePlayerName = <input type="text" required value = {playerName} onChange={handleChange} />
+        btnCaption = "Save";
     }
     return (
-        <li className={isActive ? 'active': undefined}>
-            <span className='player'>
+        <li className={isActive ? "active": undefined}>
+            <span className = "player">
                 {editablePlayerName}
-                <span className='player-symbol'>{symbol}</span>
+                <span className = "player-symbol" >{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{btnCaption}</button>
         </li>
