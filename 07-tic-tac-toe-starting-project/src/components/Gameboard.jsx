03c9b5c63f90b818b0be6458efc1131fc03f7fee
MargaretMugo <mugomargaret128@gmail.com>
2026-08-06 10:16:50 +0300

    error handling

--- 07-tic-tac-toe-starting-project/src/components/Gameboard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Gameboard.jsx
@@ -30,7 +30,8 @@ export default function Gameboard({ onSelectSquare , turns}) {
                     {row.map((playerSymbol, colIndex) =>
                         <li key={colIndex}>
                             <button onClick={() => onSelectSquare(rowIndex , colIndex)}
-                                disabled={playerSymbol !== null}>{playerSymbol}</button>
+                            disabled={playerSymbol !== null}
+                            >{playerSymbol}</button>
                         </li>)}
                 </ol>
             </li>)}
