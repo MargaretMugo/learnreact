2d90913d05da99e78f648467127a4cfc1571009e
MargaretMugo <mugomargaret128@gmail.com>
2026-08-12 12:48:56 +0300

    game over with a draw

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -43,6 +43,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns(
@@ -60,7 +62,7 @@ function App() {
           <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'} />
           <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'} />
         </ol>
-        {winner && <GameOver winner={winner}/>}
+        {(winner || hasDraw) && <GameOver winner={winner}/>}
         <Gameboard onSelectSquare={handleSelectSquare} board = {gameBoard}/>
       </div>
       <Log turns = {gameTurns}/>
