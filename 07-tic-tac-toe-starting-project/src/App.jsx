b3767b0a68b9da7c6cf1bc95fd8c5a9f2cb56346
MargaretMugo <mugomargaret128@gmail.com>
2026-08-12 09:36:00 +0300

    initialGameBoard reference error

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   //const [activePlayer, setActivePlayer] = useState('X');
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  //let gameBoard = initialGameBoard;
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
