fc8053ce866baecfaf2b79fbb5a2da4fec8faee1
MargaretMugo <mugomargaret128@gmail.com>
2026-08-12 12:00:05 +0300

    added the gameover component

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,6 +3,7 @@ import Player from "./components/Player";
 import Gameboard from "./components/Gameboard";
 import Log from './components/Log';
 import { WINNING_COMBINATIONS } from './winning-combinations';
+import GameOver from './components/Gameover';
 
 function deriveActivePlayer(gameTurns){
   let currentPlayer = 'X';
@@ -59,7 +60,7 @@ function App() {
           <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'} />
           <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'} />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {winner && <GameOver winner={winner}/>}
         <Gameboard onSelectSquare={handleSelectSquare} board = {gameBoard}/>
       </div>
       <Log turns = {gameTurns}/>
