479fd5dc724370551f4780497276478e0f843e8c
MargaretMugo <mugomargaret128@gmail.com>
2026-08-13 10:06:51 +0300

    punctuation error handling

--- 07-tic-tac-toe-starting-project/src/components/Gameover.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Gameover.jsx
@@ -1,11 +1,12 @@
 import App from "../App";
 
 export default function GameOver({winner}){
-    return 
+    return (
     <div id = "game-over">
         <h2>Game Over!</h2>
         {winner && <p> {winner} won!</p>}
         {!winner && <p>It&apos;s a draw!</p>}
         <p><button>Rematch!</button></p>
     </div>
+    );
 }
