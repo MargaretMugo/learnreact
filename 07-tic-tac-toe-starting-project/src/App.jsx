d0a794526cc2a921a5882f57ef14e466bfb6b7db
MargaretMugo <mugomargaret128@gmail.com>
2026-08-13 16:39:59 +0300

    changes to game-over screen

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -5,6 +5,12 @@ import Log from './components/Log';
 import { WINNING_COMBINATIONS } from './winning-combinations';
 import GameOver from './components/Gameover';
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+
 function deriveActivePlayer(gameTurns){
   let currentPlayer = 'X';
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
@@ -19,11 +25,7 @@ function App() {
   //const [activePlayer, setActivePlayer] = useState('X');
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ];
+  let gameBoard = [...initialGameBoard];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -53,7 +55,10 @@ function App() {
         const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns];
         return updatedTurns;
       });//...prevTurns copies the existing prevTurn);
-}
+  }
+  function handleRematch(){
+    setGameTurns([]);
+  }
   return (
     <main>
       <div id='game-container'>
@@ -62,7 +67,7 @@ function App() {
           <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'} />
           <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'} />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner}/>}
+        {(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch}/>}
         <Gameboard onSelectSquare={handleSelectSquare} board = {gameBoard}/>
       </div>
       <Log turns = {gameTurns}/>
