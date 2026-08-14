fd83165e0f501c8186dd98bad6096dd4873a8079
MargaretMugo <mugomargaret128@gmail.com>
2026-08-14 12:10:33 +0300

    added player name to gameover screen by lifting the state up

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -11,7 +11,7 @@ const initialGameBoard = [
   [null, null, null],
 ]
 
-function deriveActivePlayer(gameTurns){
+function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
     currentPlayer = 'O';
@@ -20,12 +20,16 @@ function deriveActivePlayer(gameTurns){
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    'X': 'Player 1',
+    'O': 'Player 2'
+  });
   const [gameTurns, setGameTurns] = useState([]);
   //const [hasWinner, setHasWinner] = useState(false);
   //const [activePlayer, setActivePlayer] = useState('X');
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = [...initialGameBoard.map(array=>[...array])];
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -35,13 +39,13 @@ function App() {
   }
 
   let winner;
-  for (const combination of WINNING_COMBINATIONS){
+  for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
     const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
     const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
 
-    if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol & firstSquareSymbol === thirdSquareSymbol){
-      winner = firstSquareSymbol;
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol & firstSquareSymbol === thirdSquareSymbol) {
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -56,21 +60,38 @@ function App() {
         return updatedTurns;
       });//...prevTurns copies the existing prevTurn);
   }
-  function handleRematch(){
+  function handleRematch() {
     setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName//overwriting the symbol of the player stored in symbol to their new name
+      };
+    });
   }
   return (
     <main>
       <div id='game-container'>
         {/* Players */}
         <ol id='players' className='highlight-player'>
-          <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'} />
-          <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'} />
+          <Player
+            initialName='Player 1'
+            symbol='X'
+            isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange} />
+          <Player
+            initialName='Player 2'
+            symbol='O'
+            isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange} />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch}/>}
-        <Gameboard onSelectSquare={handleSelectSquare} board = {gameBoard}/>
+        {(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch} />}
+        <Gameboard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
-      <Log turns = {gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   )
 }
